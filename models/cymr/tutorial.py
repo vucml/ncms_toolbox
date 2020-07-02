8226b760af8a6a04f1ea97e36aa4246c3980b998
Sean Polyn <sean.polyn@vanderbilt.edu>
2020-07-02 16:03:48 -0500

    working on section 4 and variable parameters

--- models/cymr/tutorial.py
+++ models/cymr/tutorial.py
@@ -66,11 +66,12 @@ plt.savefig('temp_crp.pdf')
 # evaluate likelihood of the synthetic data for each model variant
 
 # parameter sweep B_rec
+temp_param = param.copy()
 B_rec_vals = np.linspace(0,1,11)
 logl_vals = np.zeros((len(B_rec_vals),),dtype=float)
 for i in range(len(B_rec_vals)):
-    param['B_rec'] = B_rec_vals[i]
-    logl, n = model.likelihood(sim, param,
+    temp_param['B_rec'] = B_rec_vals[i]
+    logl, n = model.likelihood(sim, temp_param,
                                patterns=patterns, weights=weights)
     logl_vals[i] = logl
 
@@ -82,6 +83,53 @@ plt.plot([0.5, 0.5],[plt.ylim()[0], plt.ylim()[1]])
 plt.xlabel('beta rec value')
 plt.ylabel('log likelihood')
 plt.savefig('B_rec_recovery.pdf')
+
+# Section 4. Creating synthetic neural data and linking it to the
+# temporal context reinstatement process.  Generating synthetic
+# behavioral data using this 'neural' model.
+
+# create B_rec array with as many entries as recall events
+
+# this gets added to the param structure
+# call the likelihood function, passing in the param structure (using CMRDistributed model class)
+# Neal proposed we may want to use the study/recall data structure to store the param values
+# this would make sense, as
+# going from likelihood to likelihood_subject requires filtering the data structures
+# likelihood is line 310 in fit.py
+# args: data, group_param, subj_param
+# data struct is filtered using the 'subject' field
+# looks like prepare_sim then splits it into study events and recall events, both passed into likelihood_subject
+# then in likelihood_subject (line 222 models.py) prepare_list_param is for Lfc, Lcf, p_stop
+# if e.g. you wanted arbitrary variability based on a data struct field
+# this should presumably happen in likelihood_subject, before call to net.study
+
+# sim contains study events and recall events, trial_type says which is which
+synth_data = sim.copy()
+neural_signal_strength = 0.1
+base_val = param['B_rec']
+var_B_rec = base_val + np.random.randn(synth_data.shape[0]) * neural_signal_strength
+var_B_rec[var_B_rec<0] = 0
+var_B_rec[var_B_rec>1] = 1
+synth_data = synth_data.assign(B_rec=pd.Series(var_B_rec).values)
+# set B_rec column to NaN or None for study events
+synth_data['B_rec'][synth_data['trial_type']=='study'] = None
+
+# so now the goal is to have the simulation code check the data structure for
+# fields that match parameter names, and use the entries to set the
+# variable values of that parameter!
+
+# B can be an array for study events, also for recall events?
+# cymr/network.py
+
+# track the sequence of function calls
+# model.likelihood
+# prepare_sim(subject_data)
+# likelihood_subject(study, recall, param) fit.py->models.py CMRDistributed
+# ... net.study()
+# ... net.p_recall(~,~,param['B_rec'],...)
+# p_recall() # at this point it has B, iterates over recall events
+# so, either alter the call to the param structure
+# or alter p_recall function to treat param as vector?
 
 print('hi')
 #param_name = ['B_enc', 'B_rec']
